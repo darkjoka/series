29a3669dc2420e2cc9378112e8f508924796f56c
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-09 21:14:03 +0000

    add trailers to url paths

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,4 @@
+from os import name
 from django.urls import path
 
 from . import views
@@ -5,4 +6,5 @@ from . import views
 urlpatterns = [
     path("/", views.indexView, name="index"),
     path("search/?<str:searchTerm>", views.searchView, name="search"),
+    path("trailers/", views.trailersView, name="trailers"),
 ]
