116cc57460bda91a944b6627e333bab6872df4a3
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-09 21:34:05 +0000

    add detail to url paths

--- api/urls.py
+++ api/urls.py
@@ -5,7 +5,8 @@ from . import views
 
 urlpatterns = [
     path("/", views.indexView, name="index"),
+    path("detail/<str: movie>", views.detailView, name="detail"),
+    path("image/<str: image>", views.imageView, name="image"),
     path("search/?<str:searchTerm>", views.searchView, name="search"),
     path("trailers/", views.trailersView, name="trailers"),
-    path("image/<str: image>", views.imageView, name="image"),
 ]
