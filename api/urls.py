5b898846829541c1f058abc496492c8ed76e938b
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-09 21:50:52 +0000

    fix white space in url

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path("/", views.indexView, name="index"),
-    path("detail/<str: movie>", views.detailView, name="detail"),
-    path("image/<str: image>", views.imageView, name="image"),
+    path("detail/<str:movie>", views.detailView, name="detail"),
+    path("image/<str:image>", views.imageView, name="image"),
     path("search/?<str:searchTerm>", views.searchView, name="search"),
     path("trailers/", views.trailersView, name="trailers"),
 ]
