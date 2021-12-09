e53285d09a9964841bd44a7505b4c274268722e9
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-09 21:02:48 +0000

    add search view

--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
 from django.http import JsonResponse
 
 from scraper.index import index
+from scraper.query import genericSearch
 
 
-def indexView(request):
+def indexView(_) -> JsonResponse:
     result = index()
+    return JsonResponse({"data": result})
+
+
+def searchView(_, searchTerm) -> JsonResponse:
+    result = genericSearch(searchTerm)
     return JsonResponse({"data": result})
