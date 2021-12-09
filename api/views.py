9e8aeac00c6ed61e730701f1416650fefbc3a568
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-09 20:54:00 +0000

    add index view

--- api/views.py
+++ api/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+from scraper.index import index
+
+
+def indexView(request):
+    result = index()
+    return JsonResponse({"data": result})
