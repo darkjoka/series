aaf1df506b4d26eacd83aa5acd8b9b04fc39ce7a
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-10 03:29:28 +0000

    remove question mark

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path("", views.indexView, name="index"),
     path("detail/<str:movie>", views.detailView, name="detail"),
     path("image/<str:image>", views.imageView, name="image"),
-    path("search/?<str:searchTerm>", views.searchView, name="search"),
+    path("search/<str:searchTerm>", views.searchView, name="search"),
     path("trailers/", views.trailersView, name="trailers"),
 ]
