ff8a23c3d0bae09bb0d145a8984891e1abadae9d
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-10 00:14:51 +0000

    change default path

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/", views.indexView, name="index"),
+    path("", views.indexView, name="index"),
     path("detail/<str:movie>", views.detailView, name="detail"),
     path("image/<str:image>", views.imageView, name="image"),
     path("search/?<str:searchTerm>", views.searchView, name="search"),
