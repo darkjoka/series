9dd22fda37e7d67571460f8d34a7d109b9ba9ecd
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-10 03:53:22 +0000

    add filter to url paths

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path("", views.indexView, name="index"),
     path("detail/<str:movie>", views.detailView, name="detail"),
+    path("filter/<str:filter>", views.filterView, name="filter"),
     path("image/<str:image>", views.imageView, name="image"),
     path("search/<str:searchTerm>", views.searchView, name="search"),
     path("trailers/", views.trailersView, name="trailers"),
