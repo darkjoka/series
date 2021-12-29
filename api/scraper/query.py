617ad2017869c2972feb55b6a5c258e688082434
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2021-12-29 01:29:59 +0000

    add queryInfoseek func

--- api/scraper/query.py
+++ api/scraper/query.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from bs4.element import ResultSet
 import os
+import pickle
 import requests
 from requests.compat import quote_plus
 from requests.models import Response
@@ -9,6 +10,7 @@ from typing import List
 from . import constants as const
 from .media import image
 from .stypes import Movie
+from .detail import detail
 
 
 def genericSearch(searchTerm: str) -> List[Movie]:
@@ -20,13 +22,15 @@ def genericSearch(searchTerm: str) -> List[Movie]:
     soup: BeautifulSoup = BeautifulSoup(mime.content, const.PARSER)
     articles: ResultSet = soup.find_all("article")
 
-    return [
+    data = [
         {
             "title": article.get_text().strip(),
             "permalink": article.find("a").get("href").split("/")[-1],
         }
         for article in articles
     ]
+
+    return queryInfoSeek(const.QUERY_CACHE, data)
 
 
 def filteredSearch(filter: str) -> List[Movie]:
@@ -53,7 +57,7 @@ def filteredSearch(filter: str) -> List[Movie]:
         ),
     }
 
-    return [
+    data = [
         {
             "title": components["title"](article),
             "permalink": components["permalink"](article),
@@ -61,3 +65,36 @@ def filteredSearch(filter: str) -> List[Movie]:
         }
         for article in articles
     ]
+
+    return queryInfoSeek(const.FILTER_CACHE, data)
+
+
+def queryInfoSeek(store, data):
+    result = []
+    cached_data = {}
+
+    if not os.path.isfile(store):
+        with open(store, "wb") as file:
+            pickle.dump(dict(), file)
+
+    with open(store, "rb") as file:
+        cached_data = pickle.load(file)
+        for info in data:
+
+            if info["title"] in cached_data:
+                result.append(cached_data[info["title"]])
+            else:
+                detailData = detail(info["permalink"])
+                requiredData = {key: info[key] for key in info.keys()}
+                requiredData["heroImage"] = detailData["heroImage"]
+                description = " ".join(detailData["description"].split("\n"))
+                requiredData["description"] = description[:150]
+                requiredData["rating"] = detailData["rating"]
+
+                result.append(requiredData)
+                cached_data[info["title"]] = requiredData
+
+    with open(store, "wb") as file:
+        pickle.dump(cached_data, file)
+
+    return result
