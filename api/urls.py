a42efc0288b311c9ca457f6b2ccd171ef52533ff
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-01-01 16:00:29 +0000

    remove cursor from search

--- api/urls.py
+++ api/urls.py
@@ -9,7 +9,6 @@ urlpatterns = [
     path("filter/<str:filter>", views.filterView, {"cursor": 0}, name="filter"),
     path("filter/<str:filter>/<int:cursor>", views.filterView, name="filter"),
     path("image/<str:image>", views.imageView, name="image"),
-    path("search/<str:searchTerm>", views.searchView, {"cursor": 0}, name="search"),
-    path("search/<str:searchTerm>/<int:cursor>", views.searchView, name="search"),
+    path("search/<str:searchTerm>", views.searchView, name="search"),
     path("trailers/", views.trailersView, name="trailers"),
 ]
