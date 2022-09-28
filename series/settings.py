34d9f3a89f0b94d4567652862f5ebb756e25397b
Junior Opoku-Ansah <junioropokuansah@gmail.com>
2022-09-28 10:45:48 -0500

    allow all hosts

--- series/settings.py
+++ series/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = "1nmjju&xc7o3a4iazz1nsugoi(%r$7oe%#rc^b%vzxo$nxzc+_"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -110,7 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = "/static/"
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # heroku settings
 
